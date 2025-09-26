6e856b1dffeceeff51fe7d8654f911b0b32f052e
Pepe252525 <peter.peterlin@iu-study.org>
2025-09-26 19:36:11 +0200

    Test Klasse Main für verschiedene Anwendungstests. Später weitere Tetfälle!

---
+++ MainTest.java
@@ -0,0 +1,23 @@
+public class MainTest {
+public static void main(String []args){
+    
+    Note n1 = new Note("Einkauf","Brot,Eier,Milch");
+        System.out.println(n1);
+
+        try{
+            new Note("","Sollte Fehlschlagen");
+        }catch(IllegalArgumentException ex){
+            System.out.println("Erwarteter Fehler:" + ex.getMessage());
+        }
+          
+            n1.setContent("Käse,Wurst,Schinken");
+            System.out.println("Nach Update:"+ n1);
+            System.out.println("createdAt:"+ n1.getCreateAt());
+            System.out.println("updateAt"+ n1.getUpdateAt());
+
+        }
+
+}
+
+
+
