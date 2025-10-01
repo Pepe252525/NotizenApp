3e3d385925e0476bd1fd25e6479446a38279b383
Pepe252525 <peter.peterlin@iu-study.org>
2025-10-01 10:50:36 +0200

    CLI Menü implementieren

---
+++ notizenapp/src/CLI.java
@@ -0,0 +1,61 @@
+package notizenapp.src;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class CLI {
+
+    private final NoteManager manager;
+    private final Scanner scanner;
+
+    public CLI(NoteManager manager){
+        this.manager = manager;
+        this.scanner = new Scanner(System.in);  
+    }
+// System Schleife für die Methode Start()
+        public void start(){
+            while(true){
+                showMenu();
+                String choice = scanner.nextLine().trim();
+
+                switch(choice){
+                    case "1":
+                        addNote();
+                        break;
+                    case "2":
+                        manager.listNotes();
+                        break;
+                    case "3":
+                    searchNotes();
+                    break;
+                    case "0":
+                    System.out.println("Programm beendet. Notizen werden gespeichert!");
+                    return;
+                    default: System.out.println("Ungültige Eingabe bitte wiederholen");
+
+                }
+            }
+        } 
+// Methode ShowMenu 
+     private void showMenu(){
+        System.out.println("\n=== Notizen-App ===");
+        System.out.println("1) Neue Notiz hinzufügen");
+        System.out.println("2) Alle Notizen anzeigen");
+        System.out.println("3) Notizen durchsuchen");
+        System.out.println("0) Beenden");
+        System.out.print("Auswahl: ");
+     } 
+// Methode addNote()
+    private void addNote(){
+        System.out.print("Title: ");
+        String title = scanner.nextLine();
+        System.out.print("Inhalt: ");
+        String content = scanner.nextLine();
+        manager.addNote(title,content);
+        System.out.println("Notiz hinzugefügt");
+    }  
+// Methode searchNotes()
+    private void searchNotes(){
+
+    }       
+}
